0241747c8f8140d0fff45e6f1eb97ef81fcafadf
Poliana <polianaramos.ti@gmail.com>
2016-06-06 01:12:00 -0300

    Arquivo atualizado

---
+++ SportsStore.WebUI/Controllers/ContaController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SportsStore.WebUI.Controllers
+{
+    public class ContaController : Controller
+    {
+        // GET: Conta
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(FormCollection f)
+        {
+            if(f["txtUsuario"] == "Paulo" &&  f["txtSenha"] == "123")
+            {
+                System.Web.Security.FormsAuthentication.SetAuthCookie(f["txtUsuario"],false);
+                return Redirect("/");
+            }
+
+            return View();
+        }
+
+        public ActionResult LogOff()
+        {
+            System.Web.Security.FormsAuthentication.SignOut();
+
+            return Redirect("/Conta/Login");
+        }
+
+
+    }
+}
